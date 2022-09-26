83e744a00822c70a8f8998044b78bd5bf3b40723
forkgouden <porteiroperez+fork@gmail.com>
2022-09-26 10:32:53 +0200

    probando que todo vaya bien

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -4,4 +4,7 @@ export const prerender = true;
 export async function load({ params, fetch  }) {
   const slug = params.slug;
   const post = await fetch(`users/${slug}.json`).then((r) => r.json());
-  return await { post }};
+  return await { 
+    post 
+  }
+};
