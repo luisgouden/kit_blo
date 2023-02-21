46576c15d5a3ae587893ef8e440ef705762a82e8
luisgouden <porteiroperez+github@gmail.com>
2023-02-21 12:56:03 +0100

    Update +page.js

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,7 +1,7 @@
 export const prerender = true;
  
 /** @type {import('./$types').PageLoad} */
-export async function load({ params, fetch  }) {
+export async function load({ params, fetch }) {
   const slug = params.slug;
   const post = await fetch(`users/${slug}.json`).then((r) => r.json());
   return await { 
